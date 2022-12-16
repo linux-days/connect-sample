1d78075f49a2439ddf73080b37ae0695d1d5e497
LD2015 <linuxdays2015@gmail.com>
2022-12-16 15:22:56 +0900

    PHP PDO SQL SERVER

---
+++ php_pdo_sqlsrv.php
@@ -0,0 +1,23 @@
+<?php
+    $serverName = "sqlserver.example.com";
+    $database = "myDbName";
+    $uid = 'sqlserver_username';
+    $pwd = 'password';
+
+    try {
+        $conn = new PDO(
+            "sqlsrv:server=$serverName;Database=$database",
+            $uid,
+            $pwd,
+            array(
+                //PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            )
+        );
+        echo "<p>Connected to SQL Server</p>\n";
+    }
+    catch(PDOException $e) {
+        die("Error connecting to SQL Server: " . $e->getMessage());
+    }
+
+    
